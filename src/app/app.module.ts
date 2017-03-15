c0c32a4e77a62ce231b752ad9892107d8523c8c8
nikitavs <nikitav@senexx.com>
2017-03-15 19:12:55 +0200

    Merge new ng2 cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MaterialModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { LoginModule } from "./login/login.module";
+
+import { FacebookService } from 'ng2-facebook-sdk';
+import { IdentityService } from './identity/identity.service';
+import { IdentityActivateService } from './identity/identity-activate.service';
 
 @NgModule({
   declarations: [
@@ -13,6 +21,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
