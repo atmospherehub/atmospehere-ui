617ae8d74475eee55141b91f328a628366eaeaa9
AmirSasson <amirsas@gmail.com>
2017-03-21 09:25:21 +0200

    design

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login.component';
 
 const routes: Routes = [
-  { path: 'loginsdk', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
 
 ];
 
