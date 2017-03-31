64d2b89188b4abd7ea0b20f09d4ee4456e748cf2
Amir Sasson <amirsas@gmail.com>
2017-03-31 09:24:59 +0300

    remove high cart temporary

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,8 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChartModule } from 'angular2-highcharts';
-import * as Highcharts from 'highcharts';
-import * as HighchartsMore from 'highcharts/highcharts-more';
+// import { ChartModule } from 'angular2-highcharts';
+// import * as Highcharts from 'highcharts';
+// import * as HighchartsMore from 'highcharts/highcharts-more';
+
+// require('highcharts/highcharts-more')(Highcharts);
+
 import { DashboardComponent } from './dashboard.component';
 import { WeeklyAverageChartComponent } from '../weekly-average-chart/weekly-average-chart.component';
 import { DailyAverageChartComponent } from '../daily-average-chart/daily-average-chart.component';
@@ -11,7 +15,8 @@ import { MaterialModule } from '@angular/material';
 @NgModule({
   imports: [
     CommonModule,
-    ChartModule.forRoot(Highcharts, HighchartsMore),
+    BrowserModule,
+    // ChartModule.forRoot(require('highcharts')),
     MaterialModule
   ],
   declarations: [
