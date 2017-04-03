e01d00c2bd8b08402a57bdadee07b9d56f08e71c
Eugene Bendersky <ugn.bnd@gmail.com>
2017-04-03 12:09:58 +0300

    Fixed highcharts import

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,27 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartModule } from 'angular2-highcharts';
-import * as Highcharts from 'highcharts';
-// import * as HighchartsMore from 'highcharts/highcharts-more';
-
-// require('highcharts/highcharts-more')(Highcharts);
-
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
 import { DashboardComponent } from './dashboard.component';
 import { WeeklyAverageChartComponent } from '../weekly-average-chart/weekly-average-chart.component';
 import { DailyAverageChartComponent } from '../daily-average-chart/daily-average-chart.component';
 import { MaterialModule } from '@angular/material';
 
+// http://stackoverflow.com/questions/42636948/angular-2-aot-calling-function-chartmodule-function-calls-not-supported
+declare var require: any;
+export function highchartsFactory() {
+  const hc = require('highcharts');
+  const hcm = require('highcharts/highcharts-more');
+  hcm(hc);
+  return hc;
+}
+
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
-    // ChartModule.forRoot(require('highcharts')),
+    ChartModule,
     MaterialModule
   ],
   declarations: [
     DashboardComponent,
     WeeklyAverageChartComponent,
     DailyAverageChartComponent
-  ]
+  ],
+  providers: [
+    {
+      provide: HighchartsStatic,
+      useFactory: highchartsFactory
+    }
+  ],
 })
 export class DashboardModule { }
